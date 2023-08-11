741e391b736b8e4d234fa4a06898d2e619bcda0a
harshil-rami <ramiharshil2598@gmail.com>
2023-08-11 21:07:46 +0530

    redo changes

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description = "A CNN Application",
     long_description=long_description,
-    long_description_content = "text/markdown",
+    # long_description_content = "text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
