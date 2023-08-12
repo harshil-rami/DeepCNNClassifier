2cb50510653034088cc5729f7d0ef6377469f83f
harshil-rami <ramiharshil2598@gmail.com>
2023-08-12 16:34:57 +0530

    Setup Done

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ setuptools.setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description = "A CNN Application",
+    description="A small python package for CNN app",
     long_description=long_description,
-    # long_description_content = "text/markdown",
+    long_description_content="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker" : f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
-    package_dir={"","src"},
+    package_dir={"": "src"},
     packages=setuptools.find_packages(where="src")
 )
